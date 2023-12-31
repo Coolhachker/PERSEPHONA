4501f5257cfc22a98008298cbac321e42552ec7b
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2023-12-31 15:08:14 +0300

    в классе __Dataset создал обучающие пакеты

--- set_dataset.py
+++ set_dataset.py
@@ -1,11 +1,15 @@
 from vectorization_data import Vectorization
 from tensorflow._api.v2.strings import unicode_split
 from tensorflow.python.data import Dataset
+from tensorflow.python.data.experimental import AUTOTUNE
 
 
 class __Dataset:
     def __init__(self, path_to_file=''):
         self.seq_length = 5
+        self.batch_size = 64
+        self.buffer_size = 10000
+
         self.text = open('data/habr_data_training/file1.txt', 'rb').read().decode(encoding='utf-8')
 
         self.layers = Vectorization(self.text)
@@ -14,7 +18,7 @@ class __Dataset:
         self.dataset_from_ids = self.set_dataset()
         self.sequences = self.set_sequences()
 
-        self.dataset = self.sequences.map(self.split_input_target_data)
+        self.dataset = self.set_packets_for_train(self.sequences.map(self.split_input_target_data))
 
     def set_ids(self):
         return self.layers.ids_from_chars_layer(unicode_split(self.text, 'UTF-8'))
@@ -31,6 +35,9 @@ class __Dataset:
         target_text = sequences[1:]
 
         return input_text, target_text
+
+    def set_packets_for_train(self, dataset_: Dataset):
+        return dataset_.shuffle(self.buffer_size).batch(self.batch_size).prefetch(AUTOTUNE)
 
 
 if __name__ == '__main__':
