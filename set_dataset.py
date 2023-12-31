f040b474c9e50285f39245ca69918f40580aef33
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2023-12-31 14:50:08 +0300

    в классе __Dataset написал функции set_ids, set_dataset, set_sequences

--- set_dataset.py
+++ set_dataset.py
@@ -5,12 +5,20 @@ from tensorflow.python.data import Dataset
 
 class __Dataset:
     def __init__(self, path_to_file=''):
-        text = open('data/habr_data_training/file1.txt', 'rb').read().decode(encoding='utf-8')
-        self.layers = Vectorization(text)
-        self.all_ids = self.layers.ids_from_chars_layer(unicode_split(text, 'UTF-8'))
-        self.dataset_from_ids = Dataset.from_tensor_slices(self.all_ids)
+        self.seq_length = 100
+        self.text = open('data/habr_data_training/file1.txt', 'rb').read().decode(encoding='utf-8')
+        self.layers = Vectorization(self.text)
+        self.all_ids = self.set_ids()
+        self.dataset_from_ids = self.set_dataset()
 
-        print(list(self.dataset_from_ids.as_numpy_iterator()))
+    def set_ids(self):
+        return self.layers.ids_from_chars_layer(unicode_split(self.text, 'UTF-8'))
+
+    def set_dataset(self):
+        return Dataset.from_tensor_slices(self.all_ids)
+
+    def set_sequences(self):
+        pass
 
 
 if __name__ == '__main__':
