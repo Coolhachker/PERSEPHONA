90bb1f14dade07340af9bc8981ba18538a3ad06a
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2023-12-31 14:34:29 +0300

    В классе Vectorization написал функции ids_from_chars, chars_from_ids, text_from_ids для векторизации текста

--- vectorization_data.py
+++ vectorization_data.py
@@ -1,9 +1,25 @@
+from keras.layers import StringLookup
+from tensorflow._api.v2.strings import reduce_join
+
+
 class Vectorization:
     def __init__(self, data):
-        self.data = data
+        self.vocab = sorted(set(data))
+        self.ids_from_chars_layer = self.ids_from_chars()
+        self.chars_from_ids_layer = self.chars_from_ids()
 
     def ids_from_chars(self):
-        pass
+        return StringLookup(
+            vocabulary=list(self.vocab),
+            mask_token=None
+        )
 
     def chars_from_ids(self):
-        pass
+        return StringLookup(
+            vocabulary=self.ids_from_chars_layer.get_vocabulary(),
+            invert=True,
+            mask_token=None
+        )
+
+    def text_from_ids(self, ids):
+        return reduce_join(self.chars_from_ids_layer(ids), axis=-1)
