57eb21ebba53ad3fe2213bcd91a84ac1fde8b8f1
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-04-15 00:56:55 +0300

    написал скрипт для отката модели на чекпоинт (в будущем реализовать целый класс для такого в случае остановки обучения)

---
+++ get_saved_model_by_checkpoints.py
@@ -0,0 +1,26 @@
+from set_model import PERSEPHONA
+from vectorization_data import Vectorization
+from tensorflow._api.v2.strings import unicode_split
+from tensorflow import constant
+from tensorflow import train
+from tensorflow import saved_model
+
+
+def get_saved_model():
+    layers = Vectorization('data/habr_data_training/habr_DEVELOP.txt', 2000000)
+    vocab_size = len(layers.ids_from_chars_layer.get_vocabulary())
+    persephona = PERSEPHONA(vocab_size, 256, 1024)
+
+    first_promt = constant(['input'])
+    __input__ = layers.ids_from_chars_layer(unicode_split(first_promt, 'UTF-8'))
+    x, state = persephona(inputs=__input__, return_state=True)
+
+    latest_checkpoint = train.latest_checkpoint('checkpoints')
+
+    persephona.load_weights(latest_checkpoint)
+
+    saved_model.save(persephona, "PERSEPHONA_R")
+
+
+if __name__ == '__main__':
+    get_saved_model()
